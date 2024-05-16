48a356d4a79d03924a619c944ff585cff511bb57
supaicy <iwilldtw@gmail.com>
2024-05-16 13:46:02 +0900

    init

---
+++ src/main/java/live/smoothing/smoothingactuator/SmoothingActuatorApplication.java
@@ -0,0 +1,14 @@
+package live.smoothing.smoothingactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothingActuatorApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SmoothingActuatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/smoothing/smoothingactuator/SmoothingActuatorApplicationTests.java
@@ -0,0 +1,14 @@
+package live.smoothing.smoothingactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoothingActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
