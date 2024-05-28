1d21676b89310bfffd0f8edafc1ec09f8df1fc6e
supaicy <iwilldtw@gmail.com>
2024-05-28 09:32:47 +0900

    [Docs]: javadoc 추가

--- src/main/java/live/smoothing/actuator/checker/impl/Co2Checker.java
+++ src/main/java/live/smoothing/actuator/checker/impl/Co2Checker.java
@@ -5,6 +5,11 @@ import live.smoothing.actuator.dto.DataDTO;
 import live.smoothing.actuator.service.ConditionSettingsService;
 import org.springframework.stereotype.Component;
 
+/**
+ * 이산화탄소 데이터로 조건의 true/false 판단
+ *
+ * @author 신민석
+ */
 @Component("co2Checker")
 public class Co2Checker implements ConditionChecker {
 
