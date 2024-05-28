6cbde9f9975a9b9c9133801a81edd89cee5a337b
supaicy <iwilldtw@gmail.com>
2024-05-28 09:32:35 +0900

    [Docs]: javadoc 추가

--- src/main/java/live/smoothing/actuator/checker/ConditionChecker.java
+++ src/main/java/live/smoothing/actuator/checker/ConditionChecker.java
@@ -1,8 +1,12 @@
 package live.smoothing.actuator.checker;
 
-import live.smoothing.actuator.config.ConditionSettings;
 import live.smoothing.actuator.dto.DataDTO;
 
+/**
+ * 조건 체크 인터페이스
+ *
+ * @author 신민석
+ */
 public interface ConditionChecker {
 
     boolean checkCondition(DataDTO data);
