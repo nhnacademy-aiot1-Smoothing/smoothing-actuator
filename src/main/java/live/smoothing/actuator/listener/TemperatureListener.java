1aa871372bec6b070b78e1c20b2819b4dd9e4f8d
supaicy <iwilldtw@gmail.com>
2024-05-28 09:37:17 +0900

    [Feat]: BaseListener 수정에 따른 추가 구현
    - 제어 조건에 맞게 구현 필요

--- src/main/java/live/smoothing/actuator/listener/TemperatureListener.java
+++ src/main/java/live/smoothing/actuator/listener/TemperatureListener.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import live.smoothing.actuator.config.RabbitMQProperties;
 import live.smoothing.actuator.service.ConditionSettingsService;
 import live.smoothing.actuator.dto.DataDTO;
+import live.smoothing.actuator.service.ControlHistoryService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +17,10 @@ public class TemperatureListener extends BaseListener {
                                ApplicationContext applicationContext,
                                ConditionSettingsService conditionSettingsService,
                                ObjectMapper objectMapper,
-                               RabbitMQProperties properties) {
+                               RabbitMQProperties properties,
+                               ControlHistoryService controlHistoryService) {
 
-        super(rabbitTemplate, applicationContext, conditionSettingsService, properties, objectMapper);
+        super(rabbitTemplate, applicationContext, conditionSettingsService, properties, objectMapper, controlHistoryService);
     }
 
     @RabbitListener(queues = "temperature-queue")
